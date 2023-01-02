540a6bbc77954cd964759facde7b0803727be57e
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2023-01-02 11:34:38 +0900

    add db

--- 02_go-docker-postgres-rest/cmd/main.go
+++ 02_go-docker-postgres-rest/cmd/main.go
@@ -1,9 +1,15 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/divrhino/divrhino-trivia/database"
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
+    database.ConnectDb()
     app := fiber.New()
+
+    setupRoutes(app)
 
     app.Get("/", func(c *fiber.Ctx) error {
         return c.SendString("Hello, Takenari!")
